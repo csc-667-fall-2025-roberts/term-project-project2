6afabf2ea548ae038f52f8dc33566500b0821a3e
diego sunderland <922606643@sfsu.edu>
2025-12-19 17:17:47 -0800

    fix server error

--- Application/src/backend/sockets/init.ts
+++ Application/src/backend/sockets/init.ts
@@ -13,20 +13,32 @@ export const initSockets = (httpServer: HTTPServer) => {
   io.engine.use(sessionMiddleware);
 
   io.on("connection", (socket) => {
-    // @ts-ignore
-    const session = socket.request.session as { id: string; user: User };
-    logger.info(`socket for user session ${session.id} established`)
+    // Session may be missing for some connections; guard access.
+    const reqAny = socket.request as any;
+    const session = (reqAny?.session ?? undefined) as { id?: string; user?: User } | undefined;
 
-    socket.join(session.id);
+    if (session?.id) {
+      logger.info(`socket for user session ${session.id} established`);
+      socket.join(session.id);
+    } else {
+      logger.warn("socket connection without session id; joining global room only");
+    }
+
     socket.join(GLOBAL_ROOM);
 
     const gameId = socket.handshake.query.gameId as string;
-    if (gameId) {
+    if (gameId && session?.user?.id) {
       initGameSocket(socket, parseInt(gameId), session.user.id);
+    } else if (gameId) {
+      logger.warn("socket connection missing user; skipping game socket init");
     }
     
     socket.on("close", () => {
-      logger.info(`socket for user ${session.user.username} closed`);
+      if (session?.user?.username) {
+        logger.info(`socket for user ${session.user.username} closed`);
+      } else {
+        logger.info("socket closed");
+      }
     });
   });
 
